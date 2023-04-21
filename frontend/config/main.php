8678b120783385c827d661040b9522145f4ef5ba
User <mavrin79@mail.ru>
2023-04-21 15:46:49 +0400

    test

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -14,6 +14,7 @@ return [
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
+            'baseUrl' => '',
         ],
         'user' => [
             'identityClass' => 'common\models\User',
@@ -36,14 +37,17 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+            'enableStrictParsing' => true,
             'rules' => [
+				'/' => '/site/about',
+	            '<action:[-a-zA-Z0-9_]+>' => 'site/<action>',
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
