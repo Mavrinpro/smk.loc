4114304adcf1266567d0227015b70c01d4fcc776
keepe <mavrin79@mail.ru>
2023-04-27 19:15:21 +0400

    form

--- frontend/web/js/custom.js
+++ frontend/web/js/custom.js
@@ -6,3 +6,31 @@ $(document).ready(function (){
    )
 })
 
+$("#TooltipDemo").click(function() {
+
+    toastr.info('We do have the Kapua suite available.', 'Turtle Bay Resort', {
+        timeOut: 5000,
+        closeButton: true,
+        progressBar: true
+    })
+});
+
+
+// t(".show-toastr-example").click("(function()"{
+//     "n.a.options="{
+//         "closeButton":!0,
+//             "debug":!1,
+//             "newestOnTop":!0,
+//             "progressBar":!0,
+//             "positionClass":"toast-bottom-center",
+//             "preventDuplicates":!1,
+//             "onclick":null,
+//             "showDuration":"300",
+//             "hideDuration":"1000",
+//             "timeOut":"5000",
+//             "extendedTimeOut":"1000",
+//             "showEasing":"swing",
+//             "hideEasing":"linear",
+//             "showMethod":"fadeIn",
+//             "hideMethod":"fadeOut"
+//     }
