4cd40fbf4089acfa2f3f383313e0d95675f28ebe
User <mavrin79@mail.ru>
2023-04-27 11:17:32 +0400

    widget AlertWidget.php

--- frontend/components/widgets/views/alertwidget.php
+++ frontend/components/widgets/views/alertwidget.php
@@ -2,21 +2,21 @@
 $session = Yii::$app->session;
 
 if (\Yii::$app->session->hasFlash('success')) {
-    $language = $session->get('success');
+    $message = $session->get('success');
     $toast = "toastr.success";
 } else if (\Yii::$app->session->hasFlash('info')) {
-    $language = $session->get('info');
+    $message = $session->get('info');
     $toast = "toastr.info";
 }else if (\Yii::$app->session->hasFlash('warning')) {
-    $language = $session->get('warning');
+    $message = $session->get('warning');
     $toast = "toastr.warning";
 } else if (\Yii::$app->session->hasFlash('error')) {
-    $language = $session->get('error');
+    $message = $session->get('error');
     $toast = "toastr.error";
 }
 $js = <<<JS
 var styleToast = $toast;
-    styleToast('Уведомление будет скрыто автоматически.', '$language', {
+    styleToast('Уведомление будет скрыто автоматически.', '$message', {
         timeOut: 7000,
         closeButton: true,
         progressBar: true,
