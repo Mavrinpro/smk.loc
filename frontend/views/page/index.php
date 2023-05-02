96c3fbc356fd746a6829431c087bdf564abb390c
User <mavrin79@mail.ru>
2023-05-02 11:29:24 +0400

    class

--- frontend/views/page/index.php
+++ frontend/views/page/index.php
@@ -25,9 +25,9 @@ $this->params['breadcrumbs'][] = $this->title;
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+            //['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+            //'id',
             'name',
             'department_id',
             'create_at',
--- frontend/views/page/view.php
+++ frontend/views/page/view.php
@@ -27,27 +27,31 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('<i class="fa fa-download"></i>', ['create-doc', 'id' => $model->id], ['class' => 'btn 
         btn-light', 'title' => 'Скачать', 'data-toggle' => 'tooltip', 'data-placement' => 'top']) ?>
 
-<!--        --><?//= Html::a('<i class="fa fa-print"></i>', ['#', 'id' => $model->id], ['class' => 'btn
-//        btn-secondary', 'title' => 'Распечатать', 'data-toggle' => 'tooltip', 'data-placement' => 'top', 'onclicl'
-//            => 'window.print()']) ?>
-        <a href="#" class="btn btn-secondary" title="Печать" data-toggle="tooltip" id="docprint" onclick="window.print()"><i class="fa
+        <!--        --><? //= Html::a('<i class="fa fa-print"></i>', ['#', 'id' => $model->id], ['class' => 'btn
+        //        btn-secondary', 'title' => 'Распечатать', 'data-toggle' => 'tooltip', 'data-placement' => 'top', 'onclicl'
+        //            => 'window.print()']) ?>
+        <a href="#" class="btn btn-secondary" title="Печать" data-toggle="tooltip" id="docprint"
+           onclick="window.print()"><i class="fa
         fa-print"></i></a>
     </p>
     <div class="mb-3 card">
         <div class="card-body">
             <ul class="tabs-animated-shadow tabs-animated nav">
                 <li class="nav-item">
-                    <a role="tab" class="nav-link" id="tab-c-0" data-toggle="tab" href="#tab-animated-0" aria-selected="false">
+                    <a role="tab" class="nav-link" id="tab-c-0" data-toggle="tab" href="#tab-animated-0"
+                       aria-selected="false">
                         <span>Tab 1</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a role="tab" class="nav-link active" id="tab-c-1" data-toggle="tab" href="#tab-animated-1" aria-selected="true">
+                    <a role="tab" class="nav-link active" id="tab-c-1" data-toggle="tab" href="#tab-animated-1"
+                       aria-selected="true">
                         <span>Tab 2</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a role="tab" class="nav-link" id="tab-c-2" data-toggle="tab" href="#tab-animated-2" aria-selected="false">
+                    <a role="tab" class="nav-link" id="tab-c-2" data-toggle="tab" href="#tab-animated-2"
+                       aria-selected="false">
                         <span>Tab 3</span>
                     </a>
                 </li>
@@ -60,38 +64,44 @@ $this->params['breadcrumbs'][] = $this->title;
 
             <div class="tab-content">
                 <div class="tab-pane" id="tab-animated-0" role="tabpanel">
-                    <p class="mb-0">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+                    <p class="mb-0">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when
+                        an unknown printer took a galley of type and scrambled it to make a type specimen
                         book.
-                        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+                        It has survived not only five centuries, but also the leap into electronic typesetting,
+                        remaining essentially unchanged. </p>
                 </div>
                 <div class="tab-pane active" id="tab-animated-1" role="tabpanel">
-                    <p class="mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                    <p class="mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                        Ipsum has been the industry's standard dummy text ever since the 1500s, when an
                         unknown
-                        printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+                        printer took a galley of type and scrambled it to make a type specimen book. It has survived not
+                        only five centuries, but also the leap into electronic typesetting, remaining essentially
+                        unchanged. </p>
                 </div>
                 <div class="tab-pane" id="tab-animated-2" role="tabpanel">
-                    <p class="mb-0">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+                    <p class="mb-0">It was popularised in the 1960s with the release of Letraset sheets containing Lorem
+                        Ipsum passages, and more recently with desktop publishing software like Aldus
                         PageMaker including versions of Lorem Ipsum.</p>
                 </div>
             </div>
         </div>
     </div>
 
-<!--    --><?//= DetailView::widget([
-//        'model' => $model,
-//        'attributes' => [
-//            'id',
-//            'name',
-//            'department_id',
-//            'create_at',
-//            'update_at',
-//            'user_id_create',
-//            'user_id_update',
-//        ],
-//    ]) ?>
+    <!--    --><? //= DetailView::widget([
+    //        'model' => $model,
+    //        'attributes' => [
+    //            'id',
+    //            'name',
+    //            'department_id',
+    //            'create_at',
+    //            'update_at',
+    //            'user_id_create',
+    //            'user_id_update',
+    //        ],
+    //    ]) ?>
     <div class="row">
         <div class="col-12 mb-3" id="print">
-            <?php echo $model->text;  ?>
+            <?php echo $model->text; ?>
 
         </div>
 
