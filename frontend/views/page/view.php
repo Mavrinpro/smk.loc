673bb26d43bec615e031ee6e0a0ea0be040559b5
User <mavrin79@mail.ru>
2023-05-02 16:42:51 +0400

    history table

--- frontend/views/page/view.php
+++ frontend/views/page/view.php
@@ -35,58 +35,58 @@ $this->params['breadcrumbs'][] = $this->title;
            onclick="window.print()"><i class="fa
         fa-print"></i></a>
     </p>
-    <div class="mb-3 card">
-        <div class="card-body">
-            <ul class="tabs-animated-shadow tabs-animated nav">
-                <li class="nav-item">
-                    <a role="tab" class="nav-link" id="tab-c-0" data-toggle="tab" href="#tab-animated-0"
-                       aria-selected="false">
-                        <span>Tab 1</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a role="tab" class="nav-link active" id="tab-c-1" data-toggle="tab" href="#tab-animated-1"
-                       aria-selected="true">
-                        <span>Tab 2</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a role="tab" class="nav-link" id="tab-c-2" data-toggle="tab" href="#tab-animated-2"
-                       aria-selected="false">
-                        <span>Tab 3</span>
-                    </a>
-                </li>
-            </ul>
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalDelete">
-                Launch demo modal
-            </button>
-
-
-            <!--            <div class="tab-content">-->
-            <!--                <div class="tab-pane" id="tab-animated-0" role="tabpanel">-->
-            <!--                    <p class="mb-0">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when-->
-            <!--                        an unknown printer took a galley of type and scrambled it to make a type specimen-->
-            <!--                        book.-->
-            <!--                        It has survived not only five centuries, but also the leap into electronic typesetting,-->
-            <!--                        remaining essentially unchanged. </p>-->
-            <!--                </div>-->
-            <!--                <div class="tab-pane active" id="tab-animated-1" role="tabpanel">-->
-            <!--                    <p class="mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem-->
-            <!--                        Ipsum has been the industry's standard dummy text ever since the 1500s, when an-->
-            <!--                        unknown-->
-            <!--                        printer took a galley of type and scrambled it to make a type specimen book. It has survived not-->
-            <!--                        only five centuries, but also the leap into electronic typesetting, remaining essentially-->
-            <!--                        unchanged. </p>-->
-            <!--                </div>-->
-            <!--                <div class="tab-pane" id="tab-animated-2" role="tabpanel">-->
-            <!--                    <p class="mb-0">It was popularised in the 1960s with the release of Letraset sheets containing Lorem-->
-            <!--                        Ipsum passages, and more recently with desktop publishing software like Aldus-->
-            <!--                        PageMaker including versions of Lorem Ipsum.</p>-->
-            <!--                </div>-->
-            <!--            </div>-->
-        </div>
-    </div>
+<!--    <div class="mb-3 card">-->
+<!--        <div class="card-body">-->
+<!--            <ul class="tabs-animated-shadow tabs-animated nav">-->
+<!--                <li class="nav-item">-->
+<!--                    <a role="tab" class="nav-link" id="tab-c-0" data-toggle="tab" href="#tab-animated-0"-->
+<!--                       aria-selected="false">-->
+<!--                        <span>Tab 1</span>-->
+<!--                    </a>-->
+<!--                </li>-->
+<!--                <li class="nav-item">-->
+<!--                    <a role="tab" class="nav-link active" id="tab-c-1" data-toggle="tab" href="#tab-animated-1"-->
+<!--                       aria-selected="true">-->
+<!--                        <span>Tab 2</span>-->
+<!--                    </a>-->
+<!--                </li>-->
+<!--                <li class="nav-item">-->
+<!--                    <a role="tab" class="nav-link" id="tab-c-2" data-toggle="tab" href="#tab-animated-2"-->
+<!--                       aria-selected="false">-->
+<!--                        <span>Tab 3</span>-->
+<!--                    </a>-->
+<!--                </li>-->
+<!--            </ul>-->
+<!--             Button trigger modal -->
+<!--            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalDelete">-->
+<!--                Launch demo modal-->
+<!--            </button>-->
+<!---->
+<!---->
+<!--                        <div class="tab-content">-->
+<!--                            <div class="tab-pane" id="tab-animated-0" role="tabpanel">-->
+<!--                                <p class="mb-0">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when-->
+<!--                                    an unknown printer took a galley of type and scrambled it to make a type specimen-->
+<!--                                    book.-->
+<!--                                    It has survived not only five centuries, but also the leap into electronic typesetting,-->
+<!--                                    remaining essentially unchanged. </p>-->
+<!--                            </div>-->
+<!--                            <div class="tab-pane active" id="tab-animated-1" role="tabpanel">-->
+<!--                                <p class="mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem-->
+<!--                                    Ipsum has been the industry's standard dummy text ever since the 1500s, when an-->
+<!--                                    unknown-->
+<!--                                    printer took a galley of type and scrambled it to make a type specimen book. It has survived not-->
+<!--                                    only five centuries, but also the leap into electronic typesetting, remaining essentially-->
+<!--                                    unchanged. </p>-->
+<!--                            </div>-->
+<!--                            <div class="tab-pane" id="tab-animated-2" role="tabpanel">-->
+<!--                                <p class="mb-0">It was popularised in the 1960s with the release of Letraset sheets containing Lorem-->
+<!--                                    Ipsum passages, and more recently with desktop publishing software like Aldus-->
+<!--                                    PageMaker including versions of Lorem Ipsum.</p>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--        </div>-->
+<!--    </div>-->
 
     <!--    --><? //= DetailView::widget([
     //        'model' => $model,
