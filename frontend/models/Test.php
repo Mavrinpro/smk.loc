257608aab38304dae890ecb82daa4d75911c0df2
User <mavrin79@mail.ru>
2023-05-03 16:47:34 +0400

    test

---
+++ frontend/models/Test.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "test".
+ *
+ * @property int $id
+ * @property int|null $user_id
+ * @property string|null $result
+ * @property string|null $name
+ * @property int|null $create_at
+ * @property int|null $update_at
+ * @property int|null $action
+ */
+class Test extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'test';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'create_at', 'update_at', 'action'], 'integer'],
+            [['result'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'result' => 'Result',
+            'create_at' => 'Create At',
+            'update_at' => 'Update At',
+            'action' => 'Action',
+        ];
+    }
+}
---
+++ frontend/views/test/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\Test $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="test-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'user_id')->textInput() ?>
+
+    <?= $form->field($model, 'result')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'create_at')->textInput() ?>
+
+    <?= $form->field($model, 'update_at')->textInput() ?>
+
+    <?= $form->field($model, 'action')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
