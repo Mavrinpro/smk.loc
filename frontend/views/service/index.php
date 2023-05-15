ca1e14e81bdeb835476b28ccf103abe6d56cb4e8
User <mavrin79@mail.ru>
2023-05-15 12:57:08 +0400

    table service

---
+++ frontend/views/service/index.php
@@ -0,0 +1,53 @@
+<?php
+
+use app\models\Service;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/** @var yii\web\View $this */
+/** @var app\models\serviceSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Services';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="service-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Service', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'department_id',
+            'create_at',
+            'update_at',
+            //'user_id_create',
+            //'user_id_update',
+            //'active',
+            //'user_id',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Service $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
