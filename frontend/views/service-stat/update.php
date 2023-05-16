4714f84f51bf1b22181bde15af30f48bc54e1628
keepe <mavrin79@mail.ru>
2023-05-16 18:22:54 +0400

    migrate

--- frontend/views/service-stat/update.php
+++ frontend/views/service-stat/update.php
@@ -11,7 +11,9 @@ $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' 
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="service-stat-update">
-
+    <p>
+        <?= Html::a('Create User', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
     <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
