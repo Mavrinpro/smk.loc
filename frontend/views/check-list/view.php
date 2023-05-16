1d06e078832a96ebd28ff124935741f181efb07a
User <mavrin79@mail.ru>
2023-05-16 10:26:44 +0400

    таблица критериев оценки

--- frontend/views/check-list/view.php
+++ frontend/views/check-list/view.php
@@ -14,7 +14,6 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="check-list-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
-
     <p>
         <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('Delete', ['delete', 'id' => $model->id], [
@@ -45,4 +44,53 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
     ]) ?>
 
+</div>
+<div class="row">
+    <div class="col-md-12">
+        <table class="mb-0 table table-hover table-secondary">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Критерий</th>
+                <th>Lasik</th>
+                <th>Баллы</th>
+                <th>Номер 1</th>
+                <th>Номер 2</th>
+                <th>Номер 3</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($check as $items): ?>
+                <tr>
+                    <?php if ($items->text2 != null): ?>
+                    <td rowspan="2"><?php echo $items->id; ?></td>
+                    <td rowspan="2"><?php echo $items->name; ?></td>
+                    <td ><?php echo $items->text1; ?></td>
+                    <td><input type="text" class="form-control" placeholder="баллы"></td>
+                    <td><input type="text" class="form-control" placeholder="телефон"></td>
+                    <td><input type="text" class="form-control" placeholder="телефон"></td>
+                    <td><input type="text" class="form-control" placeholder="телефон"></td>
+                <tr>
+                    <td><?php echo $items->text2; ?></td>
+
+                    <td><input type="text" class="form-control" placeholder="баллы"></td>
+                    <td><input type="text" class="form-control" placeholder="телефон"></td>
+                    <td><input type="text" class="form-control" placeholder="телефон"></td>
+                    <td><input type="text" class="form-control" placeholder="телефон"></td>
+                </tr>
+            <?php else: ?>
+            <td><?php echo $items->id; ?></td>
+            <td><?php echo $items->name; ?></td>
+            <td><?php echo $items->text1; ?></td>
+            <td><input type="text" class="form-control" placeholder="баллы"></td>
+            <td><input type="text" class="form-control" placeholder="телефон"></td>
+            <td><input type="text" class="form-control" placeholder="телефон"></td>
+            <td><input type="text" class="form-control" placeholder="телефон"></td>
+            <?php endif; ?>
+                </tr>
+            <?php endforeach; ?>
+
+            </tbody>
+        </table>
+    </div>
 </div>
