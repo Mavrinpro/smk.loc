e6dc8bba2c0845d741ace13841ce2c121e2c2133
User <mavrin79@mail.ru>
2023-05-30 16:51:34 +0400

    fix

--- frontend/views/department/view.php
+++ frontend/views/department/view.php
@@ -11,8 +11,8 @@ use yii\bootstrap4\ActiveForm;
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
 
- $this->title = $model->name;
- $this->params['breadcrumbs'][] = ['label' => 'Отделы', 'url' => ['index']];
+$this->title = $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Отделы', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $model->name;
 \yii\web\YiiAsset::register($this);
 ?>
@@ -75,36 +75,36 @@ fa-user"></i> Добавить
         </div>
     </div>
     <?php foreach ($page as $pages) { ?>
-    <div class="col-md-3">
-        <div class="mb-3 card text-white card-body bg-white text-dark">
-            <h5 class="text-dark card-title"><?= $pages->name ?></h5>
-            With supporting text below as a natural lead-in to additional content.
+        <div class="col-md-3">
+            <div class="mb-3 card text-white card-body bg-white text-dark">
+                <h5 class="text-dark card-title"><?= $pages->name ?></h5>
+                With supporting text below as a natural lead-in to additional content.
 
-            <div class="menu-header-content btn-pane-right mt-3">
+                <div class="menu-header-content btn-pane-right mt-3">
 
-                <a href="/page/view/<?= $pages->id;  ?>" class="btn-wide btn-hover-shine btn-pill btn
+                    <a href="/page/view/<?= $pages->id; ?>" class="btn-wide btn-hover-shine btn-pill btn
                             btn-inline-block
                             btn-outline-success">Перейти
-                </a>
-                <?= Html::a('<i class="fa fa-pencil-alt"></i>', ['page/update', 'id' => $pages->id], [
-                    'class' => 'btn-wide btn-hover-shine btn-pill btn
+                    </a>
+                    <?= Html::a('<i class="fa fa-pencil-alt"></i>', ['page/update', 'id' => $pages->id], [
+                        'class' => 'btn-wide btn-hover-shine btn-pill btn
                             btn-inline-block
                             btn-outline-warning',
-                ]) ?>
+                    ]) ?>
 
-                <?= Html::a('<i class="fa fa-trash"></i>', ['page/delete', 'id' => $pages->id], [
-                    'class' => 'btn-wide btn-hover-shine btn-pill btn
+                    <?= Html::a('<i class="fa fa-trash"></i>', ['page/delete', 'id' => $pages->id], [
+                        'class' => 'btn-wide btn-hover-shine btn-pill btn
                             btn-inline-block
                             btn-outline-danger',
-                    'data' => [
-                        'confirm' => 'Хотите удалить запись ' .$pages->id.'?',
-                        'method' => 'post',
-                    ],
-                ]) ?>
+                        'data' => [
+                            'confirm' => 'Хотите удалить запись ' . $pages->id . '?',
+                            'method' => 'post',
+                        ],
+                    ]) ?>
 
+                </div>
             </div>
         </div>
-    </div>
 
     <?php } ?>
 </div>
@@ -122,16 +122,20 @@ fa-user"></i> Добавить
                                         <i class="fa fa-file-alt"></i>
                                     </div>
                                     <div class="widget-content-left flex2">
-                                        <div class="widget-heading"><a href="/page/view/<?= $pages->id;  ?>" class="widget-heading font-weight-normal"><?= $pages->name ?></a></div>
+                                        <div class="widget-heading"><a href="/page/view/<?= $pages->id; ?>"
+                                                                       class="widget-heading font-weight-normal"><?= $pages->name ?></a>
+                                        </div>
                                     </div>
 
                                     <div class="widget-content-right">
 
                                         <div class="d-inline-block dropdown">
-                                            <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="border-0 btn-transition btn btn-link">
+                                            <button type="button" data-toggle="dropdown" aria-haspopup="true"
+                                                    aria-expanded="false" class="border-0 btn-transition btn btn-link">
                                                 <i class="fa fa-ellipsis-h"></i>
                                             </button>
-                                            <div tabindex="-1" role="menu" aria-hidden="true" class="dropdown-menu dropdown-menu-right">
+                                            <div tabindex="-1" role="menu" aria-hidden="true"
+                                                 class="dropdown-menu dropdown-menu-right">
                                                 <ul class="nav flex-column">
                                                     <li class="nav-item">
                                                         <a class="nav-link">
@@ -149,7 +153,8 @@ fa-user"></i> Добавить
                                                     </li>
                                                     <?= Html::a('<i class="nav-link-icon lnr-download"></i> <span> Скачать</span>', ['page/create-doc', 'id' => $model->id], ['class' => 'nav-link', 'title' => 'Скачать']) ?>
                                                     <li class="nav-item">
-                                                        <a disabled="" data-toggle="modal" data-target="#modalDelete" class="nav-link">
+                                                        <a disabled="" data-toggle="modal" data-target="#modalDelete"
+                                                           class="nav-link">
                                                             <i class="nav-link-icon lnr-trash"></i>
                                                             <span> Удалить</span>
                                                         </a>
@@ -157,9 +162,9 @@ fa-user"></i> Добавить
                                                 </ul>
                                             </div>
                                         </div>
-<!--                                        <button class="border-0 btn-transition btn btn-outline-danger" data-toggle="modal" data-target="#modalDelete">-->
-<!--                                            <i class="fa fa-trash-alt"></i>-->
-<!--                                        </button>-->
+                                        <!--                                        <button class="border-0 btn-transition btn btn-outline-danger" data-toggle="modal" data-target="#modalDelete">-->
+                                        <!--                                            <i class="fa fa-trash-alt"></i>-->
+                                        <!--                                        </button>-->
                                     </div>
                                 </div>
                             </div>
@@ -173,18 +178,18 @@ fa-user"></i> Добавить
 <a href="" data-target="#modalCreateUser" data-toggle="modal" class="btn btn-success mb-3 mt-3"><i class="fa
 fa-user"></i> Добавить
     сотрудника</a>
-<?php if (sizeof($user) > 0){ ?>
+<?php if (sizeof($user) > 0) { ?>
     <div class="row">
         <div class="col-md-12 mt-3">
             <h3 class="mb-3">Сотрудники</h3>
-<!--            --><?//= Html::a('<i class="fa fa-user"></i> Добавить сотрудника', ['create-user', 'id' =>
-//                $model->id], ['class' => 'btn btn-success mb-3', 'data' => [
-//                'method' => 'post',
-//                'params' => [
-//                    'id' => $model->id,
-//                    'param2' => 'value2',
-//                ],
-//            ],]) ?>
+<!--                        --><?//= Html::a('<i class="fa fa-user"></i> Добавить сотрудника', ['create-user', 'id' =>
+//                            $model->id],['class' => 'btn btn-success mb-3', 'data' => [
+//                            'method' => 'post',
+//                            'params' => [
+//                                'id' => $model->id,
+//                                'param2' => 'value2',
+//                            ],
+//                        ],]) ?>
             <ul class="todo-list-wrapper list-group list-group-flush">
                 <?php
                 foreach ($user as $users) { ?>
@@ -194,13 +199,16 @@ fa-user"></i> Добавить
                             <div class="widget-content-wrapper">
                                 <div class="widget-content-left mr-2">
                                     <div class="custom-checkbox custom-control">
-                                        <input type="checkbox" id="exampleCustomCheckbox<?= $users->id ?>" class="custom-control-input">
-                                        <label class="custom-control-label" for="exampleCustomCheckbox<?= $users->id ?>">&nbsp;</label>
+                                        <input type="checkbox" id="exampleCustomCheckbox<?= $users->id ?>"
+                                               class="custom-control-input">
+                                        <label class="custom-control-label"
+                                               for="exampleCustomCheckbox<?= $users->id ?>">&nbsp;</label>
                                     </div>
                                 </div>
                                 <div class="widget-content-left">
                                     <?php if ($users->status == 10) { ?>
-                                        <a href="/user/view/<?= $users->id ?>" class="widget-heading text-success"><?= $users->username ?></a>
+                                        <a href="/user/view/<?= $users->id ?>"
+                                           class="widget-heading text-success"><?= $users->username ?></a>
                                         <div class="widget-subheading">
                                             <div>Активный
 
@@ -218,18 +226,30 @@ fa-user"></i> Добавить
                                 </div>
                                 <div class="widget-content-right widget-content-actions">
                                     <div class="d-inline-block dropdown">
-                                        <button type="button" aria-haspopup="true" data-toggle="dropdown" aria-expanded="false"
+                                        <button type="button" aria-haspopup="true" data-toggle="dropdown"
+                                                aria-expanded="false"
                                                 class="border-0 btn-transition btn btn-link">
                                             <i class="fa fa-ellipsis-h"></i>
                                         </button>
-                                        <div tabindex="-1" role="menu" aria-hidden="true" class="dropdown-menu dropdown-menu-right"
+                                        <div tabindex="-1" role="menu" aria-hidden="true"
+                                             class="dropdown-menu dropdown-menu-right"
                                              style="">
                                             <h6 tabindex="-1" class="dropdown-header">Header</h6>
-                                            <button type="button" disabled="" tabindex="-1" class="disabled dropdown-item">Action</button>
-                                            <button type="button" tabindex="0" class="dropdown-item">Another Action</button>
-                                            <button type="button" tabindex="0" class="dropdown-item">Удалить</button>
+                                            <button type="button" disabled="" tabindex="-1"
+                                                    class="disabled dropdown-item">Action
+                                            </button>
+                                            <button type="button" tabindex="0" class="dropdown-item">Another Action
+                                            </button>
+                                            <!--                                            <button type="button" tabindex="0" class="dropdown-item">Удалить</button>-->
                                             <?= Html::a('Удалить', ['department/delete-user', 'id' =>
-                                                $users->id], ['class' => 'dropdown-item']) ?>
+                                                $users->id, 'department_id' => $model->id], ['class' => 'dropdown-item',
+                                                'data' => [
+                                                'method' => 'post',
+                                                'params' => [
+                                                    'id' => 6,
+                                                    'param2' => 'value2',
+                                                ],
+                                            ],]) ?>
                                         </div>
                                     </div>
                                 </div>
