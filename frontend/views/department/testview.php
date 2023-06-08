9ec6172a1e4acdb7586a5ba6b75d0ac4e33b8fbb
User <mavrin79@mail.ru>
2023-06-08 09:30:14 +0400

    результаты теста fix

--- frontend/views/department/testview.php
+++ frontend/views/department/testview.php
@@ -1,4 +1,5 @@
 <?php
+
 use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
 use app\models\Answer;
@@ -20,33 +21,36 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="col-md-12">
             <div class="alert alert-primary"><h1>Результат теста: <?= Html::encode($user->fio) ?></h1></div>
 
-    <table class="table">
-        <thead>
-        <tr>
-            <th>Вопрос</th>
-            <th>Ответ</th>
-            <th>Дата</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php foreach ($tester as $itemq) { ?>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Вопрос</th>
+                    <th>Ответ</th>
+                    <th>Дата</th>
+                    <th><i class="fa fa-check"></i></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($tester as $itemq) { ?>
 
-            <tr>
+                    <tr>
 
-                <td><?= $itemq->question->name ?></td>
-                <?php if (isset($itemq->ans_id)): ?>
-                    <td><?= $itemq->answer->name ?> <?= $itemq->ans_id ?> <?php //var_dump($m->anser()) ?></td>
-                <?php else: ?>
-                    <td><?= $itemq->answer_text ?> </td>
-                <?php endif; ?>
-                <td><?= date('d.m.Y H:i:s', $itemq->create_at) ?> <?= $models->user->id ?></td>
+                        <td><?= $itemq->question->name ?></td>
+                        <?php if (isset($itemq->ans_id)): ?>
+                            <td><?= $itemq->answer->name ?> <?= $itemq->ans_id ?><?php //var_dump($m->anser()) ?></td>
+                        <?php else: ?>
+                            <td><?= $itemq->answer_text ?> </td>
+                        <?php endif; ?>
+                        <td><?= date('d.m.Y H:i:s', $itemq->create_at) ?> <?= $models->user->id ?></td>
+                        <td><?= (isset($itemq->ans_id)) ? (isset($itemq->answer->answer_right) &&
+                                $itemq->answer->answer_right ==
+                                1) ? '<i class="fa fa-check text-success"></i>' : '<i class="fa fa-times text-danger"></i></td>' : 'Ответ вписан' ?>
+                    </tr>
 
-            </tr>
+                <?php } ?>
 
-        <?php } ?>
-
-        </tbody>
-    </table>
+                </tbody>
+            </table>
         </div>
     </div>
 <?php
