e5c9705187a1861c50f23622526826f0af850b89
User <mavrin79@mail.ru>
2023-07-12 16:44:54 +0400

    страница логина

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -14,21 +14,12 @@ class AppAsset extends AssetBundle
     public $css = [
         'css/main.css',
         'css/site.css',
-        //'https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css'
     ];
     public $js = [
-        //'//code.jquery.com/jquery-1.11.0.min.js',
-        //'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js',
-
         'js/main.js',
         'js/toastr.js',
         '//cdn.jsdelivr.net/npm/sweetalert2@11',
         'js/jquery.editable.min.js',
-        //'https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js',
-        //'https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js',
-        //'https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js',
-        //'js/bootstrap-switch.min.js',
-        //'https://crm.glazcentre.ru/assets/c5b11da9/toastr.min.js',
         'js/custom.js',
     ];
     public $depends = [
