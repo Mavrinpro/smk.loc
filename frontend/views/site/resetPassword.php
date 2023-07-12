1bae870d036210d36c1c49d414b92707e9b3fa03
User <mavrin79@mail.ru>
2023-07-12 15:39:31 +0400

    Восстановление пароля

--- frontend/views/site/resetPassword.php
+++ frontend/views/site/resetPassword.php
@@ -7,22 +7,21 @@
 use yii\bootstrap4\Html;
 use yii\bootstrap4\ActiveForm;
 
-$this->title = 'Reset password';
+$this->title = 'Сброс пароля';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-reset-password">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please choose your new password:</p>
+    <p>Выберите новый пароль:</p>
 
     <div class="row">
         <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
 
-                <?= $form->field($model, 'password')->passwordInput(['autofocus' => true]) ?>
-
+                <?= $form->field($model, 'password')->passwordInput(['autofocus' => true])->label('Пароль') ?>
                 <div class="form-group">
-                    <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
+                    <?= Html::submitButton('Сохранить', ['class' => 'btn btn-primary']) ?>
                 </div>
 
             <?php ActiveForm::end(); ?>
