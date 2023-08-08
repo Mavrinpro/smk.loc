3fdf1aba2bea1c307632d9ae3b61eee710af402f
User <mavrin79@mail.ru>
2023-08-08 12:43:29 +0400

    passport

---
+++ frontend/views/doc/_form.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\Doc $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="doc-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'department_id')->textInput() ?>
+
+    <?= $form->field($model, 'create_at')->textInput() ?>
+
+    <?= $form->field($model, 'update_at')->textInput() ?>
+
+    <?= $form->field($model, 'user_id_create')->textInput() ?>
+
+    <?= $form->field($model, 'user_id_update')->textInput() ?>
+
+    <?= $form->field($model, 'active')->textInput() ?>
+
+    <?= $form->field($model, 'send_user_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
