66563f69302efd0da447715f5dd377b6cb17e277
User <mavrin79@mail.ru>
2023-10-10 14:49:49 +0400

    chat message table and model

---
+++ frontend/models/ChatMessage.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "chat_message".
+ *
+ * @property int $id
+ * @property int|null $message_id
+ * @property int|null $user_id
+ */
+class ChatMessage extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'chat_message';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['message_id', 'user_id'], 'integer'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'message_id' => 'Message ID',
+            'user_id' => 'User ID',
+        ];
+    }
+}
